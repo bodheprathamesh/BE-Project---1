97c0bfcd434fc2d11c7878ad091141b1022dcfe1
ankitwagh617 <95165576+ankitwagh617@users.noreply.github.com>
2024-02-22 20:57:33 +0530

    Updated frontend

--- credit-checker/src/App.js
+++ credit-checker/src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Csv from './components/Csv';
+import Form from './components/Form';
 import Body from './components/Body';
-
+import UpdateNavbar from './components/UpdateNavbar';
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Body/>
-  
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<><Navbar/><Body /></>}></Route>
+        <Route path="/fill-a-form" element={<><UpdateNavbar/><Form /></>}></Route>
+        <Route path="/upload-csv-file" element={<><UpdateNavbar/><Csv/></>}></Route>
+      </Routes>
+    </BrowserRouter>
+    {/* <Body/> */}
     </>
   );
 }
--- credit-checker/src/components/Body.js
+++ credit-checker/src/components/Body.js
@@ -1,18 +1,36 @@
-import React from 'react'
 
-import Form from "./Form"
-import Csv from "./Csv"
-import Or from "./Or"
+import React from 'react'
+import { Link } from "react-router-dom"
+
 
 export default function Body() {
+    const mystyle = {
+        color: "black",
+        fontweight : "bold",
+        padding: "10px",
+        fontFamily: "Arial",
+        textAlign:"center"
+    };
     return (
         <>
-            <div className="container my-5">
-                <Form/>
-                <Or/>
-                <Csv/>
+            <div className="container my-5" style={mystyle}>
+                <h1 >Welcome to credit checker</h1>
+                <div className="container my-5">
+                    <h3>Information</h3>
+                </div>
+                
             </div>
-
+            <div style={mystyle}>
+                <div class="d-grid gap-4 col-6 mx-auto">
+                    <Link to="/fill-a-form" class="d-grid gap-3 col-3 mx-auto">
+                        <button class="btn btn-primary" type="button">Fill a Form</button>
+                    </Link>
+                    <Link to="/upload-csv-file" class="d-grid gap-3 col-3 mx-auto">
+                        <button class="btn btn-primary" type="button">Upload CSV file</button>
+                    </Link>
+                </div>
+            </div>
         </>
     )
+    
 }
