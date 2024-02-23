bb78307733dd41745532fa05e2defcbe4ed224a3
prathamesh bodhe <pbodhe909@gmail.com>
2024-02-23 17:51:09 +0530

    csv input changes

--- Frontend/src/components/Csv.js
+++ Frontend/src/components/Csv.js
@@ -6,7 +6,7 @@ export default function Csv() {
       <div className='container my-5'>
         <form action="">
           <div className="container">
-            <label for="formFileLg" class="form-label"><h2 className='choose-title'>Select CSV file</h2></label>
+            <label for="formFileLg" class="form-label"><h2 className='choose-title'>Select CSV file from PC</h2></label>
             <input class="form-control form-control-lg my-3" id="formFileLg" type="file" accept=".csv" required/>
             <button type="submit" class="btn btn-primary my-3">Submit</button>
           </div>
